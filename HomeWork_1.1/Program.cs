298a11b227c1f5d98b83bd0b449cdf8f4c83c721
JanHapy <jan.kozhyn@gmail.com>
2022-02-03 01:12:19 +0200

    first hm

---
+++ HomeWork_1.1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a; // Initilization variable a
+            Console.WriteLine("Enter side of the squere: "); // Output message to the screne
+            a = Convert.ToInt32(Console.ReadLine()); // Reading infomation from keyboard
+            Console.WriteLine("Area is " + (a * a) + ", " + "Perimeter is " + (a + a)); // Output area & perimeter of the square
+            Console.WriteLine("This the end, have a nice day! :D ");
+            Console.ReadKey(); // This function give opportunity don't close the console window, until we press any button
+        }
+    }
+}
